5855bd850939b09cc0c8a5fe7f4deed91eacad5c
John F Hoeppner <johncom2k@gmail.com>
2025-05-04 21:32:18 +0700

    Add files via upload

--- esp32s3_fft.cpp
+++ esp32s3_fft.cpp
@@ -39,14 +39,21 @@ ESP32S3_FFT::~ESP32S3_FFT(void)
  *    total_samples = normaly the same as 'fft_size'. If larger, a sliding
  *       fft will be implemented with a 50% overlap. @note : total_samples should
  *       be an even multiple of 'fft_size'.
- * @return recalculated sample size for input buffer. 
+ * @return Pointer to a fft_table_t structure. If no memory available, returns NULL.
  */
-uint32_t ESP32S3_FFT::init(uint32_t fft_size, uint32_t total_samples)
+fft_table_t * ESP32S3_FFT::init(uint32_t fft_size, uint32_t fft_samples, uint8_t spectral_select)
 {
    uint16_t i;
-   _fft_size = fft_size;            // must be a power of 2: 64, 128, 256, 512, etc.
-   _original_samples = total_samples;  // num of samples to be fft'd
-   _hop_size = _fft_size / 2;
+   static fft_table_t fft_table;
+   _spectral_select = spectral_select;
+   _fft_size = fft_size;                  // must be a power of 2: 64, 128, 256, 512, etc.
+   _original_samples = fft_samples;     // num of samples to be fft'd
+
+   // Calculate 'hop' size
+   if(_spectral_select == SPECTRAL_NO_SLIDING)
+      _hop_size = _fft_size;          
+   else
+      _hop_size = _fft_size / 2;
 
    // Round total samples to be evenly divisible by fft_size.
    // This guarantees all samples are processed.
@@ -54,10 +61,15 @@ uint32_t ESP32S3_FFT::init(uint32_t fft_size, uint32_t total_samples)
    if(tmod == 0)           // already evenly divisible by _fft_size
       _total_samples = _original_samples;
    else 
-      _total_samples = _original_samples + (_fft_size - tmod);
-   _num_sliding_frames = (_total_samples / _hop_size) - 1;
-      Serial.printf("orig samples=%d, resized buf=%d, hop size=%d, num sliding frames=%d\n", _original_samples, _total_samples, _hop_size, _num_sliding_frames);
-   if(_num_sliding_frames < 0) _num_sliding_frames = 0;
+      _total_samples = _original_samples + (_fft_size - tmod); // 50% sliding window
+
+   // Calculate number of sliding frames
+   if(_spectral_select == SPECTRAL_NO_SLIDING)
+      _num_sliding_frames = _total_samples / _hop_size;
+   else
+      _num_sliding_frames = (_total_samples / _hop_size) - 1;
+
+   if(_num_sliding_frames < 0) _num_sliding_frames = 0;  // avoid crash
 
    // allocate memory in PSRAM for internal buffers
    if(hann_window)
@@ -71,8 +83,7 @@ uint32_t ESP32S3_FFT::init(uint32_t fft_size, uint32_t total_samples)
    fft_output = (float *) heap_caps_aligned_alloc(16, sizeof(float) * _fft_size, MALLOC_CAP_SPIRAM);
 
    if (!hann_window || !fft_buffer || !fft_output) {  // check if memory allocated OK
-      Serial.println("FFT memory alloc failed!");
-      return 0;
+      return NULL;
    } 
    
    // init the ESP32-S3 DSP engine
@@ -82,7 +93,11 @@ uint32_t ESP32S3_FFT::init(uint32_t fft_size, uint32_t total_samples)
    for (int i = 0; i < _fft_size; i++) {
       hann_window[i] = 0.5 * (1.0 - cos(2.0 * PI * i / (_fft_size - 1)));
    }     
-   return _total_samples;
+   fft_table.num_original_samples = _original_samples;
+   fft_table.hop_size = _hop_size;
+   fft_table.num_sliding_frames = _num_sliding_frames;
+   fft_table.size_input_bufr = _total_samples;
+   return &fft_table;
 }
 
 
@@ -100,7 +115,7 @@ void ESP32S3_FFT::compute(float *source_data, float *output_data)
    for (frame = 0; frame < _num_sliding_frames; frame++) {
       start = frame * _hop_size;
 
-      // Multiply input by Hann window directly & save into fft_buffer's real parts
+      // Multiply input * Hann window directly & save into fft_buffer's real parts    
       dsps_mul_f32(&source_data[start], hann_window, fft_buffer, _fft_size, 1, 1, 2);
 
       // Clear imaginary parts (odd indices) for FFT calc
@@ -108,24 +123,38 @@ void ESP32S3_FFT::compute(float *source_data, float *output_data)
          fft_buffer[2 * i + 1] = 0.0f;
       }   
 
+      // start FFT
       dsps_fft2r_fc32(fft_buffer, _fft_size);
       dsps_bit_rev_fc32(fft_buffer, _fft_size);    
 
       // compute magnitudes
-      for (j = 0; j < _fft_size / 2; j++) {
+      for (j = 0; j < _fft_size; j++) {
          float real = fft_buffer[2 * j];
          float imag = fft_buffer[2 * j + 1];
          float mag = sqrtf(real * real + imag * imag);
-         fft_output[j] += mag;
+         if(_spectral_select == SPECTRAL_AVERAGE)      // output one averaged frame
+            fft_output[j] += mag;
+         else if(_spectral_select == SPECTRAL_NO_SLIDING || _spectral_select == SPECTRAL_SLIDING)    // no frame averaging, output all sequential frames
+            output_data[(frame * _fft_size) + j] = mag;          
       }
    }   
 
    // transfer averaged FFT to 'output_data'
-   if(output_data) {
-      for(i=0; i < _fft_size / 2; i++) {
+   if(output_data && _spectral_select == SPECTRAL_AVERAGE) {
+      for(i=0; i < _fft_size; i++) {
          output_data[i] = fft_output[i] / float(_num_sliding_frames);
       }
-   } else {
-      fft_output[j] /= float(_num_sliding_frames);
    }
+}
+
+
+/********************************************************************
+ * @brief Calculate frequency resolution for compile data points. This
+ *    is analogous to the chart frequency on the X axis.
+ * 
+ * @note A simple but useful function.
+ */
+float ESP32S3_FFT::calcFreqBin(float sample_rate_hz, float fft_size)  // return freq / output data point
+{
+   return (sample_rate_hz / fft_size);
 }
--- esp32s3_fft.h
+++ esp32s3_fft.h
@@ -10,7 +10,7 @@
  * 3) This code is designed for the arduino style development environment. 
  * Developed using VSCode / PlatformIO.
  * 
- * Performance: FFT in approx 2ms for 512 samples.
+ * Performance: FFT computed in approx 2ms for 1024 samples.
  * 
  */
 #pragma once
@@ -22,23 +22,28 @@
 // FFT constants
 #define FFT_SAMPLING_FREQ  16000
 
-typedef struct {
-    uint32_t _fft_size;                   // samples in discrete fft (typical = 1024)
-    uint32_t _original_samples;           // actual number of data points to sample
-    uint32_t _total_samples;              // total samples including those that are padded
-    uint16_t _audio_sample_freq;          // typically 16KHz
-    float *_audio_source;                 // ptr to audio source data (float)
-    bool use_hann_window;
-} fft_params_t ;
+// spectral output selection
+enum {
+    SPECTRAL_AVERAGE=0,                   // output one frame of averaged fft data for all input data (default)
+    SPECTRAL_NO_SLIDING,                  // output fft frames for each block of input data
+    SPECTRAL_SLIDING,                     // output 50% sliding fft - output is 50% larger than input
+};
 
+typedef struct {
+   uint32_t num_original_samples;
+   uint32_t size_input_bufr;              // number of samples for fft input buffer
+   uint16_t num_sliding_frames;
+   uint16_t hop_size;
+} fft_table_t ;
 
 class ESP32S3_FFT {
     public:
         ESP32S3_FFT(void);
         ~ESP32S3_FFT(void);  
 
-        uint32_t init(uint32_t fft_size, uint32_t total_samples); // call on 1st use or when changing parameters
+        fft_table_t * init(uint32_t fft_size, uint32_t fft_samples, uint8_t spectral_select); // call on 1st use or when changing parameters
         void compute(float *source_data, float *output_data);  // call to perform FFT
+        float calcFreqBin(float sample_rate_hz, float fft_size);  // return freq / output data point
 
     private:
         uint32_t _fft_size;               // fft block size - in powers of 2
@@ -46,6 +51,7 @@ class ESP32S3_FFT {
         uint32_t _total_samples;          // input buffer size rounded up to multiples of _fft_size
         uint16_t _hop_size;               // sliding fft scale - use 50%
         int32_t _num_sliding_frames;      // number of frames to calc over
+        uint8_t _spectral_select;         // processing and output options - see above.
         float *hann_window;               // hann window to reduce spurious freq at start & end of input data
         float *fft_buffer;                // internal working buffer for real & imaginary FFT values
         float *fft_output;                // averaged FFT - same size as fft block
